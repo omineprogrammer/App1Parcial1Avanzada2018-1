93212e5279777fd6ca479f99fc511d11b4d2ed86
omineprogrammer <thaskete_94@hotmail.com>
2018-03-30 13:34:38 -0500

    0.2
    - metodo de dibujado con transparencia
    - funcion de dibujado con transparencia añadida a metodo de animacion por FPS

--- SnacksMachine/Form1.cs
+++ SnacksMachine/Form1.cs
@@ -12,6 +12,19 @@ namespace SnacksMachine {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+
+            /*
+            this.body.Controls.Add(this.testpic);
+            this.pictureBox2.Controls.Add(this.testpic);
+            this.stock.Controls.Add(this.testpic);
+            testpic.BackColor = System.Drawing.Color.Transparent;
+            */
+            //System.Drawing.Drawing2D.GraphicsPath gp = ;
+
+            //make transparent
+            testpic.Region = new System.Drawing.Region(Program.Transparent(testpic.Image));
+            body.Region = new System.Drawing.Region(Program.Transparent(body.Image));
+
             Program.Logger("INFO", this.Name + " initialized");
         }
 
@@ -21,17 +34,19 @@ namespace SnacksMachine {
 
         private void pictureBox2_Click(object sender, EventArgs e) {
             Animation1ChangeImg(
-                this.pictureBox1,
+                this.body,
                 new System.Drawing.Bitmap[] {
-                    global::SnacksMachine.Properties.Resources.maks_door_1,
-                    global::SnacksMachine.Properties.Resources.maks_door_2,
-                    global::SnacksMachine.Properties.Resources.maks_door_3,
-                    global::SnacksMachine.Properties.Resources.maks_door_4  
+                    global::SnacksMachine.Properties.Resources.door_2_01,
+                    global::SnacksMachine.Properties.Resources.door_2_02,
+                    global::SnacksMachine.Properties.Resources.door_2_03,
+                    global::SnacksMachine.Properties.Resources.door_2_04,
+                    global::SnacksMachine.Properties.Resources.door_2_05,
+                    global::SnacksMachine.Properties.Resources.door_2_06
                 },
-                4,
+                6,
                 false
             );          
-            Program.Logger("DEBG", pictureBox1.Name + " was clicked");
+            Program.Logger("DEBG", body.Name + " was clicked");
         }
 
         private void Animation1ChangeImg(PictureBox pictureBox, System.Drawing.Bitmap[] BitmapArr, int FramexSecond, bool continuos) {
@@ -44,19 +59,21 @@ namespace SnacksMachine {
             //handler timer
             void timer_tick(object sender, EventArgs e) {
                 timer.Stop();
-                Program.Logger("DEBG", timer.Interval + " Second cycle number " + (i + 1) + " completed");
+                Program.Logger("DEBG", timer.Interval + " MiliSecond cycle number " + (i + 1) + " completed");
                 //handler
                 if (i == (BitmapArr.Count() -1)) {
                     pictureBox.Image = BitmapArr[i];
+                    pictureBox.Region = new System.Drawing.Region(Program.Transparent(pictureBox.Image));
                     Program.Logger("DEBG", pictureBox.Name + " image change to image number " + (i + 1));
                     //again
                     if (continuos) {
-                        Program.Logger("DEBG", timer.Interval + " Second cycle again");
+                        Program.Logger("DEBG", timer.Interval + " MiliSecond cycle again");
                         i = 0;
                         timer.Start();
                     }
                 } else {
                     pictureBox.Image = BitmapArr[i];
+                    pictureBox.Region = new System.Drawing.Region(Program.Transparent(pictureBox.Image));
                     Program.Logger("DEBG", pictureBox.Name + " image change to image number " + (i + 1));
                     //next step
                     i++;
