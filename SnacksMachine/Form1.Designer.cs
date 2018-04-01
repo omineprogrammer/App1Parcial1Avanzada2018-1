a902ab2260c93c7a0d0ed3cc7bee29f1e2239fd4
omineprogrammer <thaskete_94@hotmail.com>
2018-04-01 15:22:25 -0500

    0.4
    - marcado para realizar metodo de reutilizacion de cache de region de dibujado en picturboxes
    - metodos de dragdrop añadidos, proximo a implementar
    - clase machine creada con atibuto dooropened

--- SnacksMachine/Form1.Designer.cs
+++ SnacksMachine/Form1.Designer.cs
@@ -24,34 +24,28 @@
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.bill = new System.Windows.Forms.PictureBox();
+            this.bill1 = new System.Windows.Forms.PictureBox();
             this.back = new System.Windows.Forms.PictureBox();
-            this.item4 = new System.Windows.Forms.PictureBox();
-            this.item2 = new System.Windows.Forms.PictureBox();
-            this.item3 = new System.Windows.Forms.PictureBox();
             this.moneyModule = new System.Windows.Forms.PictureBox();
-            this.row1 = new System.Windows.Forms.PictureBox();
             this.front = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.bill)).BeginInit();
+            this.buttonDoor = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bill1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.back)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.item4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.item2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.item3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.moneyModule)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.row1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.front)).BeginInit();
             this.SuspendLayout();
             // 
-            // bill
+            // bill1
             // 
-            this.bill.Image = global::SnacksMachine.Properties.Resources.bill_0004_2000;
-            this.bill.Location = new System.Drawing.Point(342, 12);
-            this.bill.Name = "bill";
-            this.bill.Size = new System.Drawing.Size(95, 52);
-            this.bill.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.bill.TabIndex = 1;
-            this.bill.TabStop = false;
-            this.bill.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.bill1.Image = global::SnacksMachine.Properties.Resources.bill_0004_2000;
+            this.bill1.Location = new System.Drawing.Point(335, 12);
+            this.bill1.Name = "bill1";
+            this.bill1.Size = new System.Drawing.Size(95, 52);
+            this.bill1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bill1.TabIndex = 1;
+            this.bill1.TabStop = false;
+            this.bill1.Click += new System.EventHandler(this.bill1_Click);
+            this.bill1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Event_MouseDown);
             // 
             // back
             // 
@@ -62,54 +56,20 @@
             this.back.Size = new System.Drawing.Size(325, 640);
             this.back.TabIndex = 0;
             this.back.TabStop = false;
-            this.back.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
-            // item4
-            // 
-            this.item4.Location = new System.Drawing.Point(471, 525);
-            this.item4.Name = "item4";
-            this.item4.Size = new System.Drawing.Size(43, 51);
-            this.item4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.item4.TabIndex = 3;
-            this.item4.TabStop = false;
-            // 
-            // item2
-            // 
-            this.item2.Location = new System.Drawing.Point(383, 525);
-            this.item2.Name = "item2";
-            this.item2.Size = new System.Drawing.Size(43, 51);
-            this.item2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.item2.TabIndex = 3;
-            this.item2.TabStop = false;
-            // 
-            // item3
-            // 
-            this.item3.Location = new System.Drawing.Point(427, 525);
-            this.item3.Name = "item3";
-            this.item3.Size = new System.Drawing.Size(43, 51);
-            this.item3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.item3.TabIndex = 3;
-            this.item3.TabStop = false;
             // 
             // moneyModule
             // 
-            this.moneyModule.BackColor = System.Drawing.Color.Transparent;
+            this.moneyModule.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(200)))), ((int)(((byte)(150)))), ((int)(((byte)(80)))));
             this.moneyModule.InitialImage = null;
-            this.moneyModule.Location = new System.Drawing.Point(265, 256);
+            this.moneyModule.Location = new System.Drawing.Point(252, 251);
             this.moneyModule.Name = "moneyModule";
-            this.moneyModule.Size = new System.Drawing.Size(56, 78);
+            this.moneyModule.Size = new System.Drawing.Size(72, 86);
             this.moneyModule.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.moneyModule.TabIndex = 3;
             this.moneyModule.TabStop = false;
-            // 
-            // row1
-            // 
-            this.row1.Location = new System.Drawing.Point(336, 522);
-            this.row1.Name = "row1";
-            this.row1.Size = new System.Drawing.Size(182, 57);
-            this.row1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.row1.TabIndex = 3;
-            this.row1.TabStop = false;
+            this.moneyModule.Click += new System.EventHandler(this.moneyModule_Click);
+            this.moneyModule.DragDrop += new System.Windows.Forms.DragEventHandler(this.Event_DragDrop);
+            this.moneyModule.DragEnter += new System.Windows.Forms.DragEventHandler(this.Event_DragEnter);
             // 
             // front
             // 
@@ -117,31 +77,36 @@
             this.front.Location = new System.Drawing.Point(4, 4);
             this.front.Name = "front";
             this.front.Size = new System.Drawing.Size(325, 640);
+            this.front.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.front.TabIndex = 2;
             this.front.TabStop = false;
             // 
+            // buttonDoor
+            // 
+            this.buttonDoor.Location = new System.Drawing.Point(383, 609);
+            this.buttonDoor.Name = "buttonDoor";
+            this.buttonDoor.Size = new System.Drawing.Size(75, 23);
+            this.buttonDoor.TabIndex = 4;
+            this.buttonDoor.Text = "Open Door";
+            this.buttonDoor.UseVisualStyleBackColor = true;
+            this.buttonDoor.Click += new System.EventHandler(this.buttonDoor_Click);
+            // 
             // Form1
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(507, 655);
-            this.Controls.Add(this.bill);
-            this.Controls.Add(this.item4);
-            this.Controls.Add(this.item2);
-            this.Controls.Add(this.item3);
+            this.Controls.Add(this.buttonDoor);
+            this.Controls.Add(this.bill1);
             this.Controls.Add(this.moneyModule);
-            this.Controls.Add(this.row1);
             this.Controls.Add(this.front);
             this.Controls.Add(this.back);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.bill)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bill1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.back)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.item4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.item2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.item3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.moneyModule)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.row1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.front)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -151,13 +116,10 @@
         #endregion
 
         private System.Windows.Forms.PictureBox back;
-        private System.Windows.Forms.PictureBox bill;
-        private System.Windows.Forms.PictureBox row1;
+        private System.Windows.Forms.PictureBox bill1;
         private System.Windows.Forms.PictureBox moneyModule;
-        private System.Windows.Forms.PictureBox item3;
-        private System.Windows.Forms.PictureBox item2;
-        private System.Windows.Forms.PictureBox item4;
         private System.Windows.Forms.PictureBox front;
+        private System.Windows.Forms.Button buttonDoor;
     }
 }
 
--- SnacksMachine/Form1.cs
+++ SnacksMachine/Form1.cs
@@ -13,43 +13,95 @@ namespace SnacksMachine {
         public Form1() {
             InitializeComponent();
 
-            this.moneyModule.DragDrop += new System.Windows.Forms.DragEventHandler(this.item1_DragDrop);
-
             //match tranparency
             Program.Transparent(this.back);
             Program.Transparent(this.front);
-            Program.Transparent(this.bill);
-            Program.Invisible(this.moneyModule);
+            Program.Transparent(this.bill1);
+            //make invisible
+            this.front.Controls.Add(this.moneyModule);
+            //Program.Invisible(this.moneyModule);
+
+            //dragdrop
+            bill1.AllowDrop = true;
+            moneyModule.AllowDrop = true;
+
+            bill1.MouseDown += new MouseEventHandler(Event_MouseDown);
+            bill1.DragDrop += new DragEventHandler(Event_DragDrop);
+            //bill1.DragEnter += new DragEventHandler(Event_DragEnter);
+
+            //moneyModule.MouseDown += new MouseEventHandler(Event_MouseDown);
+            //moneyModule.DragDrop += new DragEventHandler(Event_DragDrop);
+            moneyModule.DragEnter += new DragEventHandler(Event_DragEnter);
 
             Program.Logger("INFO", this.Name + " initialized");
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e) {
-            
+        private void bill1_Click(object sender, EventArgs e) {
+            Program.Logger("DEBG", bill1.Name + " was clicked");
         }
 
-        private void item1_DragDrop(object sender, EventArgs e) {
-            
+        private void buttonDoor_Click(object sender, EventArgs e) {
+            if (Program.machine.DoorOpened) {
+                Program.Animation1(
+                    this.front,
+                    new System.Drawing.Bitmap[] {
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0001_F07,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0002_F06,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0003_F05,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0004_F04,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0005_F03,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0006_F02,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0007_F01
+                    },
+                    15,
+                    false
+                );
+                Program.machine.DoorOpened = false;
+                buttonDoor.Text = "Open Door";
+            } else {
+                Program.Animation1(
+                    this.front,
+                    new System.Drawing.Bitmap[] {
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0006_F02,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0005_F03,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0004_F04,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0003_F05,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0002_F06,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0001_F07,
+                        global::SnacksMachine.Properties.Resources.machine2_0000s_0000_F08
+                    },
+                    15,
+                    false
+                );
+                Program.machine.DoorOpened = true;
+                buttonDoor.Text = "Close Door";
+            }
+            Program.Logger("DEBG", buttonDoor.Name + " was clicked");
         }
 
-        private void pictureBox2_Click(object sender, EventArgs e) {
-            Program.Animation1(
-                this.front,
-                new System.Drawing.Bitmap[] {
-                    global::SnacksMachine.Properties.Resources.machine2_0000s_0007_F01,
-                    global::SnacksMachine.Properties.Resources.machine2_0000s_0006_F02,
-                    global::SnacksMachine.Properties.Resources.machine2_0000s_0005_F03,
-                    global::SnacksMachine.Properties.Resources.machine2_0000s_0004_F04,
-                    global::SnacksMachine.Properties.Resources.machine2_0000s_0003_F05,
-                    global::SnacksMachine.Properties.Resources.machine2_0000s_0002_F06,
-                    global::SnacksMachine.Properties.Resources.machine2_0000s_0001_F07,
-                    global::SnacksMachine.Properties.Resources.machine2_0000s_0000_F08
-                },
-                8,
-                false
-            );          
-            Program.Logger("DEBG", back.Name + " was clicked");
+        private void moneyModule_Click(object sender, EventArgs e) {
+            Program.Logger("DEBG", moneyModule.Name + " was clicked");
         }
 
+        private void Event_MouseDown(object sender, MouseEventArgs e) {
+            PictureBox pictureBox = (PictureBox)sender;
+            if (pictureBox.Image != null) {
+                pictureBox.Select();
+                pictureBox.DoDragDrop(pictureBox.Image, DragDropEffects.Copy);
+            }
+        }
+
+        private void Event_DragDrop(object sender, DragEventArgs e) {
+            PictureBox pb = (PictureBox)sender;
+            pb.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+        }
+
+        private void Event_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.Bitmap)) {
+                e.Effect = DragDropEffects.Copy;
+            } else {
+                e.Effect = DragDropEffects.None;
+            }
+        }
     }
 }
