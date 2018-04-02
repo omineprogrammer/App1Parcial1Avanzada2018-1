fabbea4d71a479a236176d74a38d96b28b675b07
jhony2911 <37487120+jhony2911@users.noreply.github.com>
2018-04-01 21:43:01 -0500

    0.5
    - Se agregó el panel numerico con sus teclas funcionando
    - Se gregó los picturebox para exibir los productos
    - Se agregó los label con su numeración para cada producto

--- SnacksMachine/Form1.cs
+++ SnacksMachine/Form1.cs
@@ -19,6 +19,7 @@ namespace SnacksMachine {
             Program.Transparent(this.bill1);
             //make invisible
             this.front.Controls.Add(this.moneyModule);
+            this.front.Controls.Add(this.PanelPrincipal);
             //Program.Invisible(this.moneyModule);
 
             //dragdrop
@@ -53,7 +54,7 @@ namespace SnacksMachine {
                         global::SnacksMachine.Properties.Resources.machine2_0000s_0006_F02,
                         global::SnacksMachine.Properties.Resources.machine2_0000s_0007_F01
                     },
-                    15,
+                    30,
                     false
                 );
                 Program.machine.DoorOpened = false;
@@ -70,7 +71,7 @@ namespace SnacksMachine {
                         global::SnacksMachine.Properties.Resources.machine2_0000s_0001_F07,
                         global::SnacksMachine.Properties.Resources.machine2_0000s_0000_F08
                     },
-                    15,
+                    30,
                     false
                 );
                 Program.machine.DoorOpened = true;
@@ -102,6 +103,67 @@ namespace SnacksMachine {
             } else {
                 e.Effect = DragDropEffects.None;
             }
+        }
+        private void PanelPrincipal_Click(object sender, EventArgs e) {
+
+            groupBox1.Visible = true;
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            maskedTextBox1.Text = maskedTextBox1.Text + "1";
+        }
+        private void button2_Click(object sender, EventArgs e) {
+            maskedTextBox1.Text = maskedTextBox1.Text + "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            maskedTextBox1.Text = maskedTextBox1.Text + "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            maskedTextBox1.Text = maskedTextBox1.Text + "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            maskedTextBox1.Text = maskedTextBox1.Text + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+            maskedTextBox1.Text = maskedTextBox1.Text + "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e) {
+            maskedTextBox1.Text = maskedTextBox1.Text + "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e) {
+            maskedTextBox1.Text = maskedTextBox1.Text + "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e) {
+            maskedTextBox1.Text = maskedTextBox1.Text + "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e) {
+            maskedTextBox1.Text = maskedTextBox1.Text + "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e) {
+            maskedTextBox1.Clear();
+
+        }
+
+        private void button12_Click(object sender, EventArgs e) {
+            groupBox1.Visible = false;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e) {
+
         }
     }
 }
